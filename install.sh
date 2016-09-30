03f984401f053e191fd4cfd68ae5d2ae461827c8
Jair Zijp <jair@tjuna.com>
2016-09-30 10:15:52 +0200

    locale settings changed

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ fi
 
 echo "Setting locale..."
 echo "# Locale settings
-	export LANGUAGE=en_US.UTF-8
-	export LANG=en_US.UTF-8
-	export LC_ALL=en_US.UTF-8">>~/.profile
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8">>~/.profile
 
 source ~/.profile	
 
